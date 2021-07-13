924f712be1d262cc32d43061ea34406c4da44c05
lidn <312238084@qq.com>
2021-07-13 23:07:01 +0800

    This is APIs, used to wrap the caller functions

---
+++ src/scalar/boolean.rs
@@ -0,0 +1,57 @@
+use crate::{array::*, bitmap::Bitmap, datatypes::DataType};
+
+use super::Scalar;
+
+use std::sync::Arc;
+
+#[derive(Debug, Clone)]
+pub struct BooleanScalar {
+    value: bool,
+    is_valid: bool,
+    phantom: std::marker::PhantomData<bool>,
+
+}
+
+impl BooleanScalar {
+    #[inline]
+    pub fn new(v: Option<bool>) -> Self {
+        let is_valid = v.is_some();
+        Self {
+            value: v.unwrap_or_default(),
+            is_valid,
+            phantom: std::marker::PhantomData,
+        }
+    }
+
+    #[inline]
+    pub fn value(&self) -> bool {
+        self.value
+    }
+}
+
+impl Scalar for BooleanScalar {
+    #[inline]
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+    #[inline]
+    fn is_valid(&self) -> bool {
+        self.is_valid
+    }
+
+    #[inline]
+    fn data_type(&self) -> &DataType {
+        &DataType::Boolean
+    }
+
+    fn to_boxed_array(&self, length: usize) -> Box<dyn Array> {
+        if self.is_valid {
+            let values = Bitmap::from_trusted_len_iter(std::iter::repeat(self.value).take(length));
+            Box::new(BooleanArray::from_data(values, None))
+        } else {
+            Box::new(BooleanArray::new_null(length))
+        }
+    }
+
+
+}
---
+++ src/scalar/null.rs
@@ -0,0 +1,46 @@
+use crate::{array::*, datatypes::DataType};
+
+use super::Scalar;
+
+use std::sync::Arc;
+
+#[derive(Debug, Clone)]
+pub struct NullScalar {
+    phantom: std::marker::PhantomData<bool>,
+}
+
+impl NullScalar {
+    #[inline]
+    pub fn new() -> Self {
+        Self { phantom: std::marker::PhantomData}
+    }
+}
+
+impl Default for NullScalar {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Scalar for NullScalar {
+    #[inline]
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    #[inline]
+    fn is_valid(&self) -> bool {
+        false
+    }
+
+    #[inline]
+    fn data_type(&self) -> &DataType {
+        &DataType::Null
+    }
+
+    #[inline]
+    fn to_boxed_array(&self, length: usize) -> Box<dyn Array> {
+        Box::new(NullArray::from_data(length))
+    }
+
+}
